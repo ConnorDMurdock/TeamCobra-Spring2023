850c42519e6c9a3c9bdc85dc6639cf911aaaedd1
Duaa <duaa.fatima44@gmail.com>
2023-04-16 14:44:18 -0500

    added room connections to the rooms text file and class methods to rooms.java

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,111 @@
-public class Room {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Room implements Serializable {
+    public int roomID;
+    public String roomName;
+    public ArrayList<Monster> monstersList;
+    public ArrayList<Item> itemsList;
+    public Puzzle roomPuzzle;
+    public String[] roomConnections;
+    public String[] directionText;
+    public String puzzleSolvedDirectionText;
+    public String roomDescription;
+    public boolean isCheckpoint;
+
+    public Room(int roomID, String roomName, ArrayList<Monster> monstersList, ArrayList<Item> itemsList, Puzzle roomPuzzle, String[] roomConnections, String[] directionText, String puzzleSolvedDirectionText, String roomDescription, boolean isCheckpoint) {
+        this.roomID = roomID;
+        this.roomName = roomName;
+        this.monstersList = monstersList;
+        this.itemsList = itemsList;
+        this.roomPuzzle = roomPuzzle;
+        this.roomConnections = roomConnections;
+        this.directionText = directionText;
+        this.puzzleSolvedDirectionText = puzzleSolvedDirectionText;
+        this.roomDescription = roomDescription;
+        this.isCheckpoint = isCheckpoint;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public ArrayList<Monster> getMonstersList() {
+        return monstersList;
+    }
+
+    public void setMonstersList(ArrayList<Monster> monstersList) {
+        this.monstersList = monstersList;
+    }
+
+    public ArrayList<Item> getItemsList() {
+        return itemsList;
+    }
+
+    public void setItemsList(ArrayList<Item> itemsList) {
+        this.itemsList = itemsList;
+    }
+
+    public Puzzle getRoomPuzzle() {
+        return roomPuzzle;
+    }
+
+    public void setRoomPuzzle(Puzzle roomPuzzle) {
+        this.roomPuzzle = roomPuzzle;
+    }
+
+    public String[] getRoomConnections() {
+        return roomConnections;
+    }
+
+    public void setRoomConnections(String[] roomConnections) {
+        this.roomConnections = roomConnections;
+    }
+
+    public String[] getDirectionText() {
+        return directionText;
+    }
+
+    public void setDirectionText(String[] directionText) {
+        this.directionText = directionText;
+    }
+
+    public String getPuzzleSolvedDirectionText() {
+        return puzzleSolvedDirectionText;
+    }
+
+    public void setPuzzleSolvedDirectionText(String puzzleSolvedDirectionText) {
+        this.puzzleSolvedDirectionText = puzzleSolvedDirectionText;
+    }
+
+    public String getRoomDescription() {
+        return roomDescription;
+    }
+
+    public void setRoomDescription(String roomDescription) {
+        this.roomDescription = roomDescription;
+    }
+
+    public boolean isCheckpoint() {
+        return isCheckpoint;
+    }
+
+    public void setCheckpoint(boolean checkpoint) {
+        isCheckpoint = checkpoint;
+    }
+
+
+
 }
