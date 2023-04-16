8a0a2470510faa8ee21c90a09f61528a87da5976
ConnorDMurdock <connordmurdock@gmail.com>
2023-04-16 16:39:33 -0400

    First draft of the Entity, Player, and Enemy classes - ConnorDMurdock

--- src/Entity.java
+++ src/Entity.java
@@ -1,2 +1,58 @@
-public class Entity {
+public class Entity
+{
+    /* Class created by: Connor Murdock
+     * The Entity class is the parent class of anything that exists in the game and participates in combat
+     * This class contains information about the health, dealt damage, name, and current room of the entity
+     */
+
+    //Hit points is how much health the entity has. Also referred to as HP
+    protected int hitPoints;
+
+    //Damage dealt is the attack stat of the entity. This is how much damage they will deal to other entities in combat
+    protected int damageDealt;
+
+    //Name is the name of the entity
+    protected String name;
+
+    //Current room ID is the numerical ID of the room this entity is currently inside
+    protected int currentRoomID;
+
+    //Constructor
+    public Entity(int hitPoints, int damageDealt, String name, int currentRoomID) {
+        this.hitPoints = hitPoints;
+        this.damageDealt = damageDealt;
+        this.name = name;
+        this.currentRoomID = currentRoomID;
+    }
+
+    //Returns the hit points of the entity
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    //Sets this entity's hit points to the given number
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    //Returns the amount of damage this entity will deal in combat
+    public int getDamageDealt() {
+        return damageDealt;
+    }
+
+    //Sets the amount of damage this entity will deal in combat
+    public void setDamageDealt(int damageDealt) {
+        this.damageDealt = damageDealt;
+    }
+
+    //Returns the name of this entity
+    public String getName() {
+        return name;
+    }
+
+    //Sets this entity's hit points to a number equal to hit points minus incoming damage. Simulates taking damage during combat.
+    //If the entity has 100 hit points and takes 10 damage, this function will subtract 10 and make hit points equal 90
+    public void takeDamage(int damage) {
+        this.hitPoints = hitPoints - damage;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,91 @@
-public class Player extends Entity {
+import java.util.ArrayList;
+
+public class Player extends Entity
+{
+    /* Class created by: Connor Murdock
+     * The Player class extends entity. This class handles all the information regarding the player
+     * This class contains information about the remaining lives, damage reduction, and inventory of the player
+     */
+
+    //Remaining lives is the number of lives the player has before reaching a game over
+    private int remainingLives;
+
+    //Damage reduction is the percent of damage blocked by currently equipped equipment
+    //Should never be less than 0 or greater than 1
+    private float damageReduction;
+
+    //The player's inventory of items. The player can only carry 20 items at a time
+    private ArrayList<Item> playerInventory;
+
+    //The player's equipped items. The player has three spots for equipment: helmet, chest plate, and leggings.
+    //index 0 = helmet, index 1 = chest plate, index 2 = leggings
+    private Equipment[] playerEquipment;
+
+    //Constructor. The default values for the player are:
+    //remaining lives = 3
+    //damage reduction = 0
+    //current room ID = 1
+    public Player(int hitPoints, int damageDealt, String name, int currentRoomID) {
+        super(hitPoints, damageDealt, name, 1);
+        this.remainingLives = 3;
+        this.damageReduction = 0;
+        this.playerInventory = new ArrayList<>();
+        this.playerEquipment = new Equipment[3];
+    }
+
+    //Adds a given item to the player's inventory
+    //If the inventory already has 20 items in it, do not add the item because the inventory is full
+    public void addItemToInventory(Item item) {
+        if (playerInventory.size() < 20){
+            playerInventory.add(item);
+        }
+        else {
+            System.err.println("Your inventory is full!");
+        }
+    }
+
+    public Item removeItemFromInventory(String itemName) {
+        Item item = null;
+        for (int i = 0; i < playerInventory.size(); i++){
+            if (playerInventory.get(i).getItemName){
+                item = playerInventory.get(i);
+            }
+        }
+        return item;
+    }
+
+    public ArrayList<String> checkInventory() {
+        ArrayList<String> inventoryLines = new ArrayList<>();
+        for (int i = 0; i < playerInventory.size(); i++){
+            Item item = playerInventory.get(i);
+            inventoryLines.add(item.getItemName + ": " + item.getItemDescription);
+        }
+        return inventoryLines;
+    }
+
+    public int attack(){
+        return damageDealt;
+    }
+
+    public void loseLife(){
+        this.remainingLives = remainingLives - 1;
+    }
+
+    public void heal(int healthRestored){
+        int newHitPoints = hitPoints + healthRestored;
+        if (newHitPoints > 100){
+            newHitPoints = 100;
+        }
+        this.hitPoints = newHitPoints;
+    }
+
+
+    /*Sets the player's hit points to a number equal to hit points minus incoming damage. Simulates taking damage during combat.
+     * rather than just taking damage, the incoming damage is decreased by the player's current equipment
+     * The amount of damage dealt will be rounded to the nearest integer
+     * e.g. player has 100 HP and 0.5 (50%) damage reduction. Player takes 10 damage, 10 * 0.5 = 5, player only takes 5 damage. New HP is 95
+     */
+    @Override public void takeDamage(int damage) {
+        this.hitPoints = hitPoints - Math.round(damage * damageReduction);
+    }
 }
