8c1d45c23956d018f1827468da22905c23302084
Jhummel117 <67962979+Jhummel117@users.noreply.github.com>
2023-04-18 18:05:59 -0400

    Update View.java

--- src/View.java
+++ src/View.java
@@ -1,2 +1,8 @@
 public class View {
+    
+    //Jonathan Hummel
+    System.out.println("Oh champion, what is thou name?");
+    System.out.println(" Well " + player.getName() +  " are you prepared to save the people who have known nothing but fear?\n" +
+            " Are you brave enough to stand up to a god?\n" +
+            " Or will you fall like the one's before you? End his mad reign and become the beacon of hope...")
 }
