09772e70af7bc9903c88ddcfde7615588b41b0c8
Jhummel117 <67962979+Jhummel117@users.noreply.github.com>
2023-04-18 17:36:47 -0400

    Update Consumable.java

--- src/Consumable.java
+++ src/Consumable.java
@@ -1,2 +1,32 @@
-public class Consumable extends Item{
+import java.io.Serializable;
+
+public class Consumable extends Item implements Serializable {
+
+
+    int potionHP;
+
+    int remainingHP;
+
+    public Consumable(String name, int itemID, int roomID, String description, int potionHP, int remainingHP) {
+        super(name, itemID, roomID, description);
+        this.potionHP = potionHP;
+        this.remainingHP = remainingHP;
+    }
+
+   /* public void useConsumable(playerHP){
+        int h = playerHP;
+        if(h < 10){
+            playerHP = playerHP + h;
+            return playerHP;
+        }
+        else{
+            int partialHealthPotion;
+            int ht = (100 - playerHP);
+            partialHealthPotion = 10-ht;
+            playerHP = 100;
+            return playerHP;
+        }
+    }*/
+
+
 }
