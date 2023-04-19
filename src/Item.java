d9748b1428cb259877a561157869a07592664260
Jhummel117 <67962979+Jhummel117@users.noreply.github.com>
2023-04-19 15:54:53 -0400

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,41 @@
-public class Item {
+//Jonathan Hummel
+import java.io.Serializable;
+
+public class Item implements Serializable {
+
+    private String name;
+    private int itemID;
+    private int roomID;
+    private String description;
+
+    public Item(String name, int itemID, int roomID, String description) {
+        this.name = name;
+        this.itemID = itemID;
+        this.roomID = roomID;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getItemID() {
+        return itemID;
+    }
+
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
+
