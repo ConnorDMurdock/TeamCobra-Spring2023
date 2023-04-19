9d1df2f96940973afcd8c0d51df22df65e515de0
Dfatima44 <91614369+Dfatima44@users.noreply.github.com>
2023-04-19 18:09:39 -0500

    Update View.java
    Added the printRoomDesc method and author attributes.

--- src/View.java
+++ src/View.java
@@ -3,10 +3,30 @@ public class View {
 
 
 
+/**
+     * displays items, puzzles and monsters in the room
+     */
 
+ //Duaa Fatima
+    public void printScanroom(Room room){
+        room.scanRoom();
+    }
     public void printRoomScan(Room room){
         room.scanRoom();
     }
 
+/**
+     * displays items, puzzles and monsters in the room
+     */
 
+    //Duaa Fatima
+    public void printRoomDesc(Room room){
+        room.enterRoomText();
+    }
+
+    
+    
+    
+    
+    
 }
