e0281a0dfc64ab58732749f5267d2628b1628bd4
Jhummel117 <67962979+Jhummel117@users.noreply.github.com>
2023-04-19 15:55:23 -0400

    Update UseItem.java

--- src/UseItem.java
+++ src/UseItem.java
@@ -1,2 +1,18 @@
-public class UseItem extends Item{
+//Jonathan Hummel
+import java.io.Serializable;
+
+public class UseItem extends Item implements Serializable {
+
+    private int damage;
+    private int equipmentSlot;
+
+    public UseItem(String name, int itemID, int roomID, String description, int damage, int equipmentSlot) {
+        super(name, itemID, roomID, description);
+        this.damage = damage;
+        this.equipmentSlot = equipmentSlot;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 }
