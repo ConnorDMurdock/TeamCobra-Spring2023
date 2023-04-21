19742a1afba748857fbd2e47bb840f49eaa697eb
Jhummel117 <67962979+Jhummel117@users.noreply.github.com>
2023-04-20 20:34:25 -0400

    Update View.java

--- src/View.java
+++ src/View.java
@@ -18,11 +18,11 @@ public class View {
 
 
  //Jonathan Hummel
-    public void introductionPart1(String name) {
+    public void introductionPart1() {
         System.out.println("Oh champion, what is thou name?");
     }
 
-    public void introductionPart2() {
+    public void introductionPart2(String name) {
         System.out.println(" Well " + name + " are you prepared to save the people who have known nothing but fear?\n" +
                 " Are you brave enough to stand up to a god?\n" +
                 " Or will you fall like the one's before you? End his mad reign and become the beacon of hope...");
