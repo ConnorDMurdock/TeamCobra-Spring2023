4a7513e8808a9e43423ac872493a590d01a5a699
Jhummel117 <67962979+Jhummel117@users.noreply.github.com>
2023-04-22 20:23:03 -0400

    Update Equipment.java

--- src/Equipment.java
+++ src/Equipment.java
@@ -4,16 +4,16 @@ import java.io.Serializable;
 public class Equipment extends Item implements Serializable {
 
     private int equipmentSlot;
-    private float damageReduction;
+    private float itemStat;
 
-    public Equipment(String name, int itemID, String description, int equipmentSlot, float damageReduction) {
+    public Equipment(String name, int itemID, String description, int equipmentSlot, float itemStat) {
         super(name, itemID, description);
         this.equipmentSlot = equipmentSlot;
-        this.damageReduction = damageReduction;
+        this.itemStat = itemStat;
     }
 
-    public float getDamageReduction() {
-        return damageReduction;
+    public float getItemStat() {
+        return itemStat;
     }
     public int getEquipmentSlot(){
         return equipmentSlot;
